189f54f78cac450b83a55c0ff5e24c74e1b5f047
chanhue <chanhue467@gmail.com>
2024-04-02 22:49:51 +0900

    [feature-map] add Map(core model)

---
+++ core/src/main/java/com/example/core/model/Map.kt
@@ -0,0 +1,21 @@
+package com.example.core.model
+
+data class Map(
+    val lastBuildDate: String,
+    val total: Int,
+    val start: Int,
+    val display: Int,
+    val items: List<LocationInfo>
+)
+
+data class LocationInfo(
+    val title: String,
+    val link: String,
+    val category: String,
+    val description: String?,
+    val telephone: String?,
+    val address: String,
+    val roadAddress: String,
+    val mapx: String,
+    val mapy: String
+)
