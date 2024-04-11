3b3d04178748ef0f94c0192e534e03c7606f4b27
chanhue <chanhue467@gmail.com>
2024-04-11 17:35:14 +0900

    [feature-FCM] FCM setting

---
+++ app/src/main/java/com/example/footballmanager_pj/NotificationActionReceiver.kt
@@ -0,0 +1,17 @@
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class NotificationActionReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent?.action) {
+            "ACCEPT_ACTION" -> {
+                Toast.makeText(context, "Accepted", Toast.LENGTH_SHORT).show()
+            }
+            "DECLINE_ACTION" -> {
+                Toast.makeText(context, "Declined", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
