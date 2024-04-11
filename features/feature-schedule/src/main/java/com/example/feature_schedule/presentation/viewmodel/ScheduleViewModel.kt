f3163d4cb5d1ba17eb004d5f6ba829b7958c8bbd
lyh990517 <lyh990517@naver.com>
2024-04-11 22:07:52 +0900

    Apply Id

--- features/feature-schedule/src/main/java/com/example/feature_schedule/presentation/viewmodel/ScheduleViewModel.kt
+++ features/feature-schedule/src/main/java/com/example/feature_schedule/presentation/viewmodel/ScheduleViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.core.ResultState.MakeClubScheduleResult
+import com.example.core.datasource.UserLocalDataSource
 import com.example.core.model.ClubSchedule
 import com.example.feature_schedule.domain.usecase.MakeScheduleUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,11 +13,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ScheduleViewModel @Inject constructor(
-    private val makeScheduleUseCase: MakeScheduleUseCase
+    private val makeScheduleUseCase: MakeScheduleUseCase,
+    private val userLocalDataSource: UserLocalDataSource
 ) : ViewModel() {
-    fun makeClub(teamId: Long, clubSchedule: ClubSchedule) = viewModelScope.launch {
-        val result = makeScheduleUseCase(teamId, clubSchedule)
-        when(result) {
+    fun makeClub(clubSchedule: ClubSchedule) = viewModelScope.launch {
+        Log.e("123", "id: ${userLocalDataSource.getSelectedTeamId()}, schedule: $clubSchedule")
+        val result = makeScheduleUseCase(userLocalDataSource.getSelectedTeamId(), clubSchedule)
+        when (result) {
             is MakeClubScheduleResult.Success -> {
                 Log.e("makeClub", "${result.code}")
             }
